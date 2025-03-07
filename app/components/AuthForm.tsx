8d35f5da618fb20768668f3570374154dd0c5441
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-03-07 22:38:55 +0300

    core: supabase client initialization and authform modified

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 export default function AuthForm(){
     const [isNewUser, setIsNewUser] = useState(false);
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [isSigningIn, setIsSigningIn] = useState(false);
     const [isSigningUp, setIsSigningUp] = useState(false);
-    const [password, setPassword] = useState("");
 
     async function handleSignUp(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault();
@@ -28,22 +28,22 @@ export default function AuthForm(){
     const signUpMessage = <p className="text-center text-white">Email sent! Check your email to confirm sign up.</p>
 
     return (
-        <form onSubmit={isNewUser ? handleSignUp : handleLogIn } className="space-y-8 mt-12 border border-gray-300 p-8 rounded-lg shadow-lg max-w-2xl mx-auto hover:border-green-400">
+        <form onSubmit={isNewUser ? handleSignUp : handleLogIn } className="space-y-8 mt-12 border border-gray-300 p-8 rounded-lg shadow-lg max-w-lg mx-auto hover:border-green-400 flex justify-center flex-col items-center">
             <input 
                 type="email"
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"            />
+                className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:text-white text-black"            />
             <input 
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder={isNewUser? "Create a password" : "Enter your password"}
-                className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"            />
+                className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:text-white text-black"            />
             <button 
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-1/4 flex items-center justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 {signInMessage}
             </button>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative">
         <div className="relative">
           <div className="absolute inset-0 bg-gold-200/20 blur-sm rounded-lg"></div>
           <div className="relative z-10">
